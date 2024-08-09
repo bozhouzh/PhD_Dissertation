aa3bb81936f60b2b45789b01ad53049c609210bb
bozhouzh <bozhouzh@usc.edu>
2024-08-09 10:35:49 -0700

    Update int_fun.m

--- Chapter_4/int_fun.m
+++ Chapter_4/int_fun.m
@@ -16,46 +16,6 @@ for iT = 1:nOptT
     UAtTimesData = UAtTimesData + a*interp1(TITShiftef(:,1), TITShiftef(:,2), timesData);
 end
 
-
 F = trapz(timesData, UAtTimesData.^2);
 
-
 end
-
-
-
-
-
-
-
-% % % 
-% % % 
-% % % 
-% % % function F = int_fun(x0)
-% % % 
-% % % a = x0(1);
-% % % t0 = x0(2);
-% % % 
-% % % global TU0 TU45
-% % % 
-% % % 
-% % % TU45Shiftef = TU45;
-% % % TU45Shiftef(:,1) = TU45(:,1) + t0;
-% % % 
-% % % timesData = 4.5e-4:1e-5:5.5e-4;
-% % % 
-% % % TU0AtTimesData = interp1(TU0(:,1), TU0(:,2), timesData);
-% % % TU45AtTimesData = interp1(TU45Shiftef(:,1), TU45Shiftef(:,2), timesData);
-% % % 
-% % % % figure(1)
-% % % % plot(timesData, TU0AtTimesData, 'b')
-% % % % hold on
-% % % % plot(timesData, a*TU22_5AtTimesData, 'r')
-% % % 
-% % % 
-% % % 
-% % % 
-% % % F = trapz(timesData, (TU0AtTimesData + a*TU45AtTimesData).^2);
-% % % 
-% % % 
-% % % end
